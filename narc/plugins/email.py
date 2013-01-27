6dbf3775f53a063550e26a767e35ad868f6b1201
Jason Corbett <jasoncorbett@gmail.com>
2013-01-26 22:42:52 -0700

    narc starts up, initializes, and listens for messages shutdown / restart plugin added

--- narc/plugins/email.py
+++ narc/plugins/email.py
@@ -3,6 +3,19 @@ The email responder plugin.  This plugin sends emails whenever a testrun is crea
 """
 __author__ = 'jcorbett'
 
+import configparser
+import logging
+
+from ..amqp import AMQPConnection
+
 class EmailResponder(object):
     """The email auto responder plugin"""
+
+    def __init__(self, configuration, amqpcon):
+        assert(isinstance(configuration, configparser.ConfigParser))
+        assert(isinstance(amqpcon, AMQPConnection))
+        self.configuration = configuration
+        self.amqpcon = amqpcon
+        self.logger = logging.getLogger('narc.plugins.email.EmailResponder')
+        self.logger.debug("Email Responder initialized with amqp connection url {}", amqpcon.url)
 
--- setup.py
+++ setup.py
@@ -16,11 +16,12 @@ setup(
     packages=find_packages(exclude=['distribute_setup']),
     package_data={'': ['*.txt', '*.rst', '*.html']},
     include_package_data=True,
-    install_requires=['slickqa>=2.0.10', 'kombu>=2.5.4', 'Jinja2>=2.6', 'pygal>=0.13.0', 'CairoSVG>=0.5', 'tinycss>=0.3', 'cssselect>=0.7.1', 'amqp>=1.0.6'],
+    install_requires=['slickqa>=2.0.11', 'kombu>=2.5.4', 'Jinja2>=2.6', 'pygal>=0.13.0', 'CairoSVG>=0.5', 'tinycss>=0.3', 'cssselect>=0.7.1', 'amqp>=1.0.6', 'anyjson>=0.3.3'],
     author="Slick Developers",
     url="http://code.google.com/p/slickqa",
     entry_points={
         'console_scripts': ['narc = narc.main:main', 'narcctl = narc.main:ctlmain'],
-        'narc.response' : ['emailresponder = narc.plugins.email:EmailResponder']
+        'narc.response' : ['emailresponder = narc.plugins.email:EmailResponder',
+                           'shutdownrestart = narc.plugins.internal:ShutdownRestartPlugin']
     }
 )
