7627da488fb30a95ce16fd73f4c511959c2e9a46
Jason Corbett <jasoncorbett@gmail.com>
2013-01-27 23:39:20 -0700

    It sends email!  Incredibly lame emails, and only to me, but it sends emails!

--- narc/plugins/email.py
+++ narc/plugins/email.py
@@ -14,6 +14,14 @@ from slickqa import micromodels
 from kombu import Consumer, Queue
 from kombu.transport.base import Message
 
+# smtp imports
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+
+
 class TestrunUpdateMessage(micromodels.Model):
     before = micromodels.ModelField(Testrun)
     after = micromodels.ModelField(Testrun)
@@ -53,6 +61,44 @@ class EmailResponder(object):
         update = TestrunUpdateMessage.from_dict(body)
         if update.before.finished is False and update.after.finished is True:
             self.logger.info("Testrun with id {} and name {} just finished.", update.after.id, update.after.name)
+            text = """
+            <h1>Testrun {testrun.name} Results</h1>
+            Total tests: {testrun.summary.total}
+            PASS: {testrun.summary.resultsByStatus.PASS}
+            """.format(testrun=update.after)
+            subject = "Results for {testrun.name}".format(testrun=update.after)
+            to = "jasoncorbett@gmail.com"
+            self.mail(to, subject, text)
+
+    def mail(self, to, subject, text):
+        # build a list from 'to'
+        to = to.strip()
+        to = to.split(',')
+        msg = MIMEMultipart()
+        msg['From'] = self.email_settings.sender
+        msg['To'] = ', '.join(to)
+        msg['Subject'] = subject
+        msg.attach(MIMEText(text, 'html'))
+
+        #if os.path.exists(image):
+        #    fp = open(image, 'rb')
+        #    img = MIMEImage(fp.read())
+        #    fp.close()
+        #    img.add_header('Content-ID', '<result.png>')
+        #    msg.attach(img)
+
+        mailServer = None
+        if self.email_settings.ssl:
+            mailServer = smtplib.SMTP_SSL(self.email_settings.smtpHostname, port=self.email_settings.smtpPort)
+        else:
+            mailServer = smtplib.SMTP(self.email_settings.smtpHostname, port=self.email_settings.smtpPort)
+        mailServer.ehlo()
+        if self.email_settings.smtpUsername is not None and self.email_settings.smtpUsername != "":
+            mailServer.login(self.email_settings.smtpUsername, self.email_settings.smtpPassword)
+        mailServer.sendmail(self.email_settings.sender, to, msg.as_string())
+        # Should be mailServer.quit(), but that crashes...
+        mailServer.close()
+
 
 
 
